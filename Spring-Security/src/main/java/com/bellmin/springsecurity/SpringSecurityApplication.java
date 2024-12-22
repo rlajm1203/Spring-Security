ae78942aaa7689942235edc242c79748fe2b8b17
bellmin <whdlas011203@naver.com>
2024-12-22 16:25:17 +0900

    chore: 프로젝트 초기 설정
    - SpringBoot 3.4.1
    - Lombok
    - MySQL
    - SpringData JPA

---
+++ Spring-Security/src/main/java/com/bellmin/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bellmin.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
